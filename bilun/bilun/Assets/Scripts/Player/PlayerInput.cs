66086d89f7572fdd53d8cfdf31d148974dd02111
safonce <saf.once28@gmail.com>
2019-09-24 17:35:48 +0800

    Add player input script to handle movement & action input

---
+++ bilun/bilun/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpInput { get; private set; }
+
+    public void UpdateInput()
+    {
+        MoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        JumpInput = Input.GetButton("Jump");
+    }
+}
