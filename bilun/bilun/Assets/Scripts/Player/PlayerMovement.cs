352839ff2797be91cf1f7c8ac140efc20dcf98c0
safonce <saf.once28@gmail.com>
2019-09-26 21:54:40 +0800

    Rotate toward direction input, add animator controller and animate player

--- bilun/bilun/Assets/Scripts/Player/PlayerMovement.cs
+++ bilun/bilun/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    private PlayerInput _playerInput;
-    private Rigidbody _rigidbody;
+    private PlayerInput playerInput;
+    private Rigidbody playerRigidbody;
+    private Animator animator;
 
     void Awake()
     {
-        _playerInput = GetComponent<PlayerInput>();
-        _rigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        _playerInput.UpdateInput();
+        playerInput.UpdateInput();
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = GetMovementDirection() * moveSpeed;
+        Vector3 direction = GetMovementDirection();
 
-        _rigidbody.MovePosition(transform.position + movement * Time.fixedDeltaTime);
+        Vector3 movement = direction * moveSpeed;
+
+        playerRigidbody.MovePosition(transform.position + movement * Time.fixedDeltaTime);
+
+        if (direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, angle, 0);
+
+            animator.SetFloat("speed", direction.sqrMagnitude);
+        }
     }
 
     Vector3 GetMovementDirection()
     {
-        Vector3 movementDirection = new Vector3(_playerInput.MoveInput.x, 0, _playerInput.MoveInput.y);
+        Vector3 movementDirection = new Vector3(playerInput.MoveInput.x, 0, playerInput.MoveInput.y);
 
         if (movementDirection.sqrMagnitude > 1f)
         {
