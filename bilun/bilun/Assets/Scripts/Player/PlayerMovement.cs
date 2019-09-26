e8024b8adda32ea49d5fc70dfc30938741f9f176
safonce <saf.once28@gmail.com>
2019-09-26 22:02:39 +0800

    Add direction based on camera

--- bilun/bilun/Assets/Scripts/Player/PlayerMovement.cs
+++ bilun/bilun/Assets/Scripts/Player/PlayerMovement.cs
@@ -27,20 +27,27 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = direction * moveSpeed;
 
+        //  Move character
         playerRigidbody.MovePosition(transform.position + movement * Time.fixedDeltaTime);
 
         if (direction != Vector3.zero)
         {
+            // Rotate towards direction
             float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, angle, 0);
 
+            //  Animate character
             animator.SetFloat("speed", direction.sqrMagnitude);
         }
     }
 
+    //  Get movement direction based on camera direction and normalized
     Vector3 GetMovementDirection()
     {
         Vector3 movementDirection = new Vector3(playerInput.MoveInput.x, 0, playerInput.MoveInput.y);
+
+        movementDirection = Camera.main.transform.TransformDirection(movementDirection);
+        movementDirection.y = 0;
 
         if (movementDirection.sqrMagnitude > 1f)
         {
