8d003e2433129c71175f2214e592034bc2b2a905
safonce <saf.once28@gmail.com>
2019-09-29 16:47:52 +0800

    Change axis, jump string to public variable string

--- bilun/bilun/Assets/Scripts/Player/PlayerInput.cs
+++ bilun/bilun/Assets/Scripts/Player/PlayerInput.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    public string HorizontalAxis = "Horizontal1";
+    public string VerticalAxis = "Vertical1";
+    public string Jump = "Jump";
+
     public Vector2 MoveInput { get; private set; }
     public bool JumpInput { get; private set; }
 
     public void UpdateInput()
     {
-        MoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MoveInput = new Vector2(Input.GetAxis(HorizontalAxis), Input.GetAxis(VerticalAxis));
 
-        JumpInput = Input.GetButtonDown("Jump");
+        JumpInput = Input.GetButtonDown(Jump);
     }
 }
