f4877f87b93c7066b88f1558f8c1c4e98583712d
safonce <saf.once28@gmail.com>
2019-10-01 22:07:16 +0800

    Add weapon data scriptable object class

---
+++ bilun/bilun/Assets/Scripts/Weapon/WeaponData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "WeaponData", menuName = "WeaponData" )]
+public class WeaponData : ScriptableObject
+{
+    public WeaponType weaponType = WeaponType.Melee;
+    public int damage = 1;
+
+    public Vector3 holdPosition = Vector3.zero;
+    public Vector3 holdRotation = Vector3.zero;
+}
+
+public enum WeaponType {
+    Melee,
+    Ranged
+}
