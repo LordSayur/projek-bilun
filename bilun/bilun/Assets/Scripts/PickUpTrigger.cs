25cab5c903c2e369ec4a3dacc3087c687aefe69e
safonce <saf.once28@gmail.com>
2019-10-01 22:14:17 +0800

    Add pick up trigger script for weapon only (testing)

---
+++ bilun/bilun/Assets/Scripts/PickUpTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpTrigger : MonoBehaviour
+{
+    public Weapon weapon;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerAttack playerAttack = other.GetComponent<PlayerAttack>();
+            playerAttack.Equip(weapon);
+        }
+    }
+}
