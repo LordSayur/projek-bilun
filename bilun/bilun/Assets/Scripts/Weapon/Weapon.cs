6bf57892e2f1062e4493d4bd8b05cc8b63f47e1b
safonce <saf.once28@gmail.com>
2019-10-01 22:07:42 +0800

    Add weapon script

---
+++ bilun/bilun/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public WeaponData weaponData;
+
+   public void Attack(PlayerAttack attacker)
+   {
+       if (weaponData.weaponType == WeaponType.Melee)
+       {
+           attacker.animator.SetTrigger("melee");
+       }
+       else
+       {
+           Debug.Log("Phew! Phew!");
+       }
+   }
+}
