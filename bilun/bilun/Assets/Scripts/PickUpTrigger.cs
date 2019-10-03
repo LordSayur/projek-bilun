3c620cabd00f89eb6087f9aebf904958561d5ae9
safonce <saf.once28@gmail.com>
2019-10-03 22:17:16 +0800

    Add isCarry boolean variable

--- bilun/bilun/Assets/Scripts/PickUpTrigger.cs
+++ bilun/bilun/Assets/Scripts/PickUpTrigger.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PickUpTrigger : MonoBehaviour
 {
     public Weapon weapon;
+    public bool isCarry {get;set;}
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isCarry)
         {
             PlayerAttack playerAttack = other.GetComponent<PlayerAttack>();
             playerAttack.Equip(weapon);
+            isCarry = true;
         }
     }
 }
