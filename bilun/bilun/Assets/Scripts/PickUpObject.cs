6ed76ec9d7189abe0c787ecb97f0f52010605fff
safonce <saf.once28@gmail.com>
2019-10-03 23:14:16 +0800

    Remove pick trigger & add pick object & weapon object

---
+++ bilun/bilun/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickUpObject : MonoBehaviour
+{
+    public bool IsCarryByPlayer {get;set;}
+    public abstract void OnTriggerEnter(Collider other);
+}
