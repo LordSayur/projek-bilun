3a0842194b87c2f361a982a1ead2fbf1d25e1483
safonce <saf.once28@gmail.com>
2019-10-03 21:19:09 +0800

    Add destroy over time script

---
+++ bilun/bilun/Assets/Scripts/DestroyOverTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    public float delayTime = 5f;
+
+    void Start()
+    {
+        Destroy(this.gameObject, delayTime);
+    }
+
+}
