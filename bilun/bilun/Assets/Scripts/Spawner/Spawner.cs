e67e5b689009bf64e5f21535c55ad262172fe9e1
Afifah Ilyana <AfifahIlyana01@gmail.com>
2019-10-04 21:55:33 +0800

    Pickup Spawner
    Added pickup spawner to prototype scene

---
+++ bilun/bilun/Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    
+    private void Awake() {
+        // For the numbers of players is two, parameter should be passed from player selection in main menu
+        GetComponentInChildren<PlayerSpawner>().SpawnPlayers(2);
+        GetComponentInChildren<PickupSpawner>().SpawnPickups();
+    }
+    
+
+}
