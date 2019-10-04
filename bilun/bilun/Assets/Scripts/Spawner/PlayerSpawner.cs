f8fa2c0f6f4f7f5bfe88c601cb3f19747441fbe4
Afifah Ilyana <AfifahIlyana01@gmail.com>
2019-10-04 21:55:33 +0800

    Pickup Spawner
    Added pickup spawner to prototype scene

---
+++ bilun/bilun/Assets/Scripts/Spawner/PlayerSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+    
+    public GameObject[] players;
+    private Transform[] spawnPos;
+
+    // players.length < spawnPos.length
+    
+    public void SpawnPlayers(int playersJoined) {
+        if(spawnPos == null)
+            spawnPos = GetComponentsInChildren<Transform>();
+
+        if (players.Length < spawnPos.Length) {
+            List<int> occupiedPos = new List<int>();
+
+            for (int i = 0; i < playersJoined; i++) {
+
+                int r = 0;
+
+                do {
+
+                    r = Random.Range(1, spawnPos.Length);
+
+                } while (occupiedPos.Contains(r));
+
+                occupiedPos.Add(r);
+
+                GameObject clonePlayer = Instantiate(players[i]);
+                clonePlayer.transform.position = new Vector3(spawnPos[r].position.x, spawnPos[r].position.y, spawnPos[r].position.z);
+
+            }
+
+            occupiedPos.Clear();
+
+        } else {
+            Debug.LogError("Not enough player spawn position for the given players size");
+        }
+    }
+}
