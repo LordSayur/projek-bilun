23bdb29c83b3c39b58991e0d01b760e70cebdcbe
safonce <saf.once28@gmail.com>
2019-10-10 21:11:35 +0800

    Add pick up item script to handle pick up item. Add function for speed change

---
+++ bilun/bilun/Assets/Scripts/PickUp/PickUpItemObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItemObject : PickUpObject
+{
+    public ItemData itemData;
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (itemData.attributeType == AttributeType.Speed)
+            {
+                PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+                if (playerMovement == null)
+                    return;
+
+                playerMovement.ChangeSpeed(itemData.changeValue, itemData.duration);
+            }            
+
+            Destroy(gameObject);
+        }
+    }
+}
