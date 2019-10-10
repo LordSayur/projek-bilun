36e01f5d7381ec753e1022c5fc93086e08a9eab5
safonce <saf.once28@gmail.com>
2019-10-10 21:10:12 +0800

    Add item data scriptable script to handle any attribute changes

---
+++ bilun/bilun/Assets/Scripts/PickUp/Items/ItemData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "ItemData", menuName = "ItemData" )]
+public class ItemData : ScriptableObject
+{
+    public AttributeType attributeType = AttributeType.None;
+    public float changeValue = 0f;
+    public float duration = 0f;
+}
+
+public enum AttributeType
+{
+    None,
+    Speed
+}
