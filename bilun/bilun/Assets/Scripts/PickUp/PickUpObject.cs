bcd3732f1c2b5c701cc271b8d2f2f161669d3bea
safonce <saf.once28@gmail.com>
2019-11-03 17:14:40 +0800

    Update prefabs with new models & rearrange prefabs

--- bilun/bilun/Assets/Scripts/PickUp/PickUpObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PickUpObject : MonoBehaviour
-{
-    public bool IsCarryByPlayer {get;set;}
-    public abstract void OnTriggerEnter(Collider other);
-}
