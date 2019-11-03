5d7f88d3af9c5d9e5ab646b8642ecc750fa3ea45
safonce <saf.once28@gmail.com>
2019-11-03 17:13:05 +0800

    Add reusable timer

---
+++ bilun/bilun/Assets/Scripts/Utilities/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : Singleton<Timer>
+{
+    public List<TimerObject> timers = new List<TimerObject>();
+
+    void Update()
+    {
+        if (timers.Count > 0)
+        {
+            for (int i = 0; i < timers.Count; i++)
+            {
+                if (Time.time > timers[i].timer)
+                {
+                    timers[i].action();
+
+                    timers.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    public void AddToTimer(float duration, TimerObject.ExecutableAction method)
+    {
+        TimerObject t = new TimerObject();
+        t.timer = Time.time + duration;
+        t.action = method;
+
+        timers.Add(t);
+    }
+}
---
+++ bilun/bilun/Assets/Scripts/Utilities/TimerObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TimerObject
+{
+    public float timer = 0f;
+
+    public delegate void ExecutableAction();
+    public ExecutableAction action;
+}
