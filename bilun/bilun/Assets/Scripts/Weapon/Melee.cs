10c9782fd6a2cdc169fee04592b93493dea76f26
safonce <saf.once28@gmail.com>
2019-11-05 20:54:10 +0800

    Add on trigger collision if hit ballon then destroy balloon and handle open & close collider during swinging

--- bilun/bilun/Assets/Scripts/Weapon/Melee.cs
+++ bilun/bilun/Assets/Scripts/Weapon/Melee.cs
@@ -4,8 +4,51 @@ using UnityEngine;
 
 public class Melee : Weapon, ISwingable
 {
-    public void Swing(PlayerAttack attacker)
+    bool hasHit = false;
+
+    public void Swing(PlayerAttack actor)
     {
-        attacker.animator.SetTrigger("melee");
+        actor.animator.SetTrigger("melee");
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {  
+        base.OnTriggerEnter(other);
+
+        if (boxCollider.isTrigger == true)
+        {
+            if (other.CompareTag("Balloon") && !hasHit)
+            {
+                hasHit = true;
+                BalloonController balloon = other.GetComponent<BalloonController>();
+                if (balloon == null)
+                    return;
+
+                balloon.DestroyBallon();
+            }
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {  
+        if (boxCollider.isTrigger == true)
+        {
+            if (collision.gameObject.CompareTag("Balloon"))
+            {
+                hasHit = false;
+            }
+        }
+    }
+
+    public void OpenCollider()
+    {
+        boxCollider.enabled = true;
+        boxCollider.isTrigger = true;
+    }
+
+    public void CloseCollider()
+    {
+        boxCollider.enabled = false;
+        boxCollider.isTrigger = false;
     }
 }
