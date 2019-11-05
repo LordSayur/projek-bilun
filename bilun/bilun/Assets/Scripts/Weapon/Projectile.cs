533e140072aeaf3dea4370065f86e13e950b1238
safonce <saf.once28@gmail.com>
2019-11-05 20:49:54 +0800

    Add projectile script to trigger collision with balloon

---
+++ bilun/bilun/Assets/Scripts/Weapon/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Balloon"))
+        {
+            BalloonController balloon = other.GetComponent<BalloonController>();
+            balloon.DestroyBallon();
+
+            Destroy(gameObject);
+        }
+    }
+}
