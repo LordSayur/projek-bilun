a1b6564d533ed51374f5eeaadb5ccf1f32c96b5a
safonce <saf.once28@gmail.com>
2019-11-05 20:50:24 +0800

    Add player health script to handle health

---
+++ bilun/bilun/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    int currentBalloon = 0;
+
+    public GameObject splashEffect;
+
+    void Awake()
+    {
+        currentBalloon = GetComponentsInChildren<BalloonController>().Length;
+    }
+
+    public void TakeDamage()
+    {
+        currentBalloon--;
+
+        if (currentBalloon <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (splashEffect != null)
+            Instantiate(splashEffect, transform.position + Vector3.up * 0.65f, Quaternion.identity);
+
+        gameObject.SetActive(false);
+    }
+}
