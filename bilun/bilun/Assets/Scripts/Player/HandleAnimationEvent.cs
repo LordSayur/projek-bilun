cf3b6b55bb1b9c12b7e49be3a8140e3bad420d94
safonce <saf.once28@gmail.com>
2019-11-05 20:56:56 +0800

    Add script to handle animation events

---
+++ bilun/bilun/Assets/Scripts/Player/HandleAnimationEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleAnimationEvent : MonoBehaviour
+{
+    PlayerAttack playerAttack;
+
+    void Awake()
+    {
+        playerAttack = GetComponentInParent<PlayerAttack>();
+    }
+
+    public void OpenDamage()
+    {
+        playerAttack.OpenWeaponCollider();
+    }
+
+    public void CloseDamage()
+    {
+        playerAttack.CloseWeaponCollider();
+    }
+}
