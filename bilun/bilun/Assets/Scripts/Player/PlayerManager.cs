7edf78acdf74a4f8f20291dae81e0e6960269fe3
safonce <saf.once28@gmail.com>
2019-11-05 22:53:40 +0800

    Add player manager to handle reset, enable & disable player, store round win

---
+++ bilun/bilun/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    PlayerHealth playerHealth;
+    PlayerMovement playerMovement;
+    PlayerAttack playerAttack;
+    PlayerAction playerAction;
+
+    public int roundWin { get; set; }
+
+    Transform SpawnPointT;
+
+    void Awake()
+    {
+        playerHealth = GetComponent<PlayerHealth>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerAttack = GetComponent<PlayerAttack>();
+        playerAction = GetComponent<PlayerAction>();
+    }
+
+    public void ResetPlayer()
+    {
+        playerHealth.Reactivate();
+
+        transform.position = SpawnPointT.position;
+        transform.rotation = SpawnPointT.rotation;
+    }
+
+    public void DisablePlayer()
+    {
+        playerMovement.enabled = false;
+        playerAttack.enabled = false;
+        playerAction.enabled = false;
+    }
+
+    public void EnablePlayer()
+    {
+        playerMovement.enabled = true;
+        playerAttack.enabled = true;
+        playerAction.enabled = true;
+    }
+
+    public void Setup(Transform SpawnPointT)
+    {
+        this.SpawnPointT = SpawnPointT;
+    }
+}
